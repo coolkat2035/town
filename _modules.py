1971dc2ef7e412996ac2863b8dff284e80968ef6
coolkat2035 <randomkat2035@gmail.com>
2022-03-19 22:41:10 +0800

    FINALLY SCROLLING YEY

--- _modules.py
+++ _modules.py
@@ -1,6 +1,10 @@
 #used to import modules to main, i'm just lazy
-import pygame, os
+
 from _rooms import *
 from _node import Node
 from _objects import *
 from _spritesheet import spritesheet
+
+#What external libraries YOU need:
+#pygame
+#pillow
